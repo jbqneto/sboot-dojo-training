e5dd5a3f4bf64e7a06cda5373dbcf54b21dff182
jbqneto <jbqneto@gmail.com>
2020-08-30 23:19:40 -0300

    Adicionado ingrediente

---
+++ src/main/java/com/jbqneto/dojo/training/domain/Ingredient.java
@@ -0,0 +1,13 @@
+package com.jbqneto.dojo.training.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@AllArgsConstructor
+@Data
+public class Ingredient {
+	
+	private String name;
+	private Long quantity;
+
+}
