5c341dd4a942404655327cc849f003d9edd025bf
jbqneto <jbqneto@gmail.com>
2020-09-07 10:31:01 -0300

    Aula 10 - Cria Conexao mysql (usei um container e banco que ja existiam)

--- src/main/java/com/jbqneto/dojo/training/domain/Recipe.java
+++ src/main/java/com/jbqneto/dojo/training/domain/Recipe.java
@@ -1,16 +1,22 @@
 package com.jbqneto.dojo.training.domain;
 
-import java.util.List;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
 @AllArgsConstructor
+@Entity
 public class Recipe {
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private int id;
-	private String name;
-	private List<Ingredient> ingredients;
+	
+	private final String name;
 
 }
